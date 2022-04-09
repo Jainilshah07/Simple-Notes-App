daf48676dd311d9f4dcae8d359817808f7ff04e2
Jainil <shahjainilb@gmail.com>
2022-04-09 17:52:46 +0530

    Basic Frontend Ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import { Home } from './Components/Home';
+import { About } from './Components/About';
 
 function App() {
   return (
+    <>
+    <BrowserRouter>
     <Navbar/>
+    <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route exact path="/about" element={<About />}>
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  </>
   );
 }
 
